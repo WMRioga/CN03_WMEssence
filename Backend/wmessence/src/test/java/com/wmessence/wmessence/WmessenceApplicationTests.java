6f30f7b1daa03ad1ebea6c9c9ab0f61556e8ea19
Weimar M Alvarez C <wm.alvarez.c@outlook.com>
2022-11-18 15:55:52 -0500

    CN03_G02_03
    Backend Spring-boot en VS Code.

---
+++ Backend/wmessence/src/test/java/com/wmessence/wmessence/WmessenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmessence.wmessence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmessenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
