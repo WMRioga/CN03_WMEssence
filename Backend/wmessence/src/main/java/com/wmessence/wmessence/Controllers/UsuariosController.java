4d43781b96868cacacfe31a94b4167c788fc1d68
Weimar M Alvarez C <wm.alvarez.c@outlook.com>
2022-11-21 15:51:00 -0500

    CN03_G02_06
    Se agrega el CRUD/Usuarios - (R).
    UsuariosController
    UsuariosRepository
    UsuariosServices

---
+++ Backend/wmessence/src/main/java/com/wmessence/wmessence/Controllers/UsuariosController.java
@@ -0,0 +1,34 @@
+package com.wmessence.wmessence.Controllers;
+
+import com.wmessence.wmessence.Models.UsuariosModel;
+import com.wmessence.wmessence.Services.UsuariosServices;
+import com.wmessence.wmessence.Repositories.UsuariosRepository;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@CrossOrigin ("*")
+@RequestMapping ("/usuarios")
+public class UsuariosController {
+    @Autowired
+    private UsuariosRepository usuariosRepository;
+
+    @Autowired
+    private UsuariosServices usuariosServices;
+
+    @GetMapping ("/list")
+    public List<UsuariosModel> consultarTodo(){
+        return usuariosServices.findAll();
+    }
+    @GetMapping ("/list/{id}")
+    public UsuariosModel consultarPorId (@PathVariable Integer id){
+        return usuariosServices.findById(id);
+    }
+}
---
+++ Backend/wmessence/src/main/java/com/wmessence/wmessence/Services/UsuariosServices.java
@@ -0,0 +1,33 @@
+package com.wmessence.wmessence.Services;
+
+import com.wmessence.wmessence.Models.UsuariosModel;
+import com.wmessence.wmessence.Repositories.UsuariosRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UsuariosServices {
+    @Autowired
+    private UsuariosRepository usuariosRepository;
+
+    @Transactional (readOnly = false)
+    public UsuariosModel save(UsuariosModel usuariosModel){
+        return usuariosRepository.save(usuariosModel);
+    }
+    @Transactional (readOnly = false)
+    public void delete (Integer id){
+        usuariosRepository.deleteById(id);
+    }
+    @Transactional (readOnly = true)
+    public UsuariosModel findById(Integer id){
+        return usuariosRepository.findById(id).orElse(null);
+    }
+    @Transactional (readOnly = true)
+    public List <UsuariosModel> findAll(){
+        return (List<UsuariosModel>) usuariosRepository.findAll();
+    }
+}
