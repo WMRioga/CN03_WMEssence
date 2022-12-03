2f6911c16c22c3d44c2291a38925e0347ab2af35
WMRioga <wm.alvarez.c@outlook.com>
2022-12-03 00:38:22 -0500

    CN03_G02_10
    Creación Front - Productos.

---
+++ Front/scripts/ProductosScript.js
@@ -0,0 +1,34 @@
+console.log("Hola Mundo")
+
+function ListarProductos(){
+    fetch("http://localhost:8080/productos/list")
+    .then(response => response.json())
+    .then(data => {
+        let codigo =""
+        console.log(data)
+        data.forEach(element => {
+            console.log(element.product_name)
+            // codigo += element.product_name + ", "
+            codigo += '<div class="col-4" style="max-width: 540px;">'
+            codigo += '    <div class="card mb-2 p-2 shadow">'
+            codigo += '        <div class="row g-0">'
+            codigo += '            <div class="col-md-4">'
+            codigo += '                <img src="img/Camisetas/CF'+element._id+'.png" class="img-fluid rounded-start" alt="...">'
+            codigo += '            </div>'
+            codigo += '            <div class="col-md-8">'
+            codigo += '            <div class="card-body">'
+            codigo += '                <h5 class="card-title">'+element.product_name+'</h5>'
+            codigo += '                <p class="card-text">'+element.product_description+'</p>'
+            codigo += '                <h6 class="card-title"> $'+element.product_cost+'</h6>'
+            codigo += '                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>'
+            codigo += '            </div>'
+            codigo += '            </div>'
+            codigo += '        </div>'
+            codigo += '    </div>'
+            codigo += '</div>'
+        });
+        document.getElementById("Productos").innerHTML = codigo
+    })
+}
+ListarProductos()
+
